8f993de8c2ff128b694ffc0da6b47fe7696a2437
kaorouguo <z2726803905@outlook.com>
2022-08-12 21:05:14 +0800

    add settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,20 @@
 import sys
 import pygame
-class AlienInvasion:
-    def __init__(self):
-        pygame.init()
+from settings import Settings
 
-        self.screen = pygame.display.set_mode((1200,800))
+
+class AlienInvasion:
+
+    def __init__(self):
+        # 初始化#
+
+        pygame.init()
+        # screen setting#
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (230,230,230)
 
     def run_game(self):
         while True:
@@ -14,9 +22,10 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 
+
 if __name__ == '__main__':
-    ai =AlienInvasion()
+    ai = AlienInvasion()
     ai.run_game()
