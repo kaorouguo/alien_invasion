1edfcca8d7f512435612d6a684432c21416a6071
kaorouguo <z2726803905@outlook.com>
2022-08-15 20:10:59 +0800

    complete move

--- ship.py
+++ ship.py
@@ -1,13 +1,29 @@
 import pygame
 
+
 class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('images/Elysia.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
+
+
+        # actor mark
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
+
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
